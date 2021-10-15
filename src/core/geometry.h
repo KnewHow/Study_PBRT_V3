c3de37aed1865dcd2e74339ee24a0a3963f42f19
Knewhow <how.yuangh@gmail.com>
2021-10-15 17:00:20 +0800

    point

--- src/core/geometry.h
+++ src/core/geometry.h
@@ -228,18 +228,245 @@ class Point2 {
 public:
     Point2(): x(0), y(0){}
     Point2(T x, T y): x(x), y(y){}
+    explicit Point2(const Point3<T>& p): x(p.x), y(p.y) { DCHECK(!HasNaNs()); }
     Point2(const Point2<T> &p) {
         DCHECK(!p.HasNaNs());
         x = p.x;
         y = p.y;
     }
-    bool HasNaNs() const {return isNaN(x) || isNaN(y); }
+    bool HasNaNs() const { return isNaN(x) || isNaN(y); }
+    template <typename U>
+    explicit Point2(const Point2<U> &p) {
+        x = (T)p.x;
+        y = (T)p.y;
+        DCHECK(!HasNaNs());
+    }
+    template <typename U>
+    explicit Point2(const Vector2<U> &v) {
+        x = (T)v.x;
+        y = (T)v.y;
+        DCHECK(!HasNaNs());
+    }
+    template <typename U>
+    explicit operator Vector2<U>() const {
+        return Vector2<U>(x, y);
+    }
+    Point2<T> &operator=(const Point2<T> &p) {
+        DCHECK(!p.HasNaNs());
+        x = p.x;
+        y = p.y;
+        return *this;
+    }
+    Point2<T> operator+(const Point2<T> &p) const {
+        DCHECK(!p.HasNaNs());
+        return Point2<T>(x + p.x, y + p.y);
+    }
+    Point2<T> operator+(const Vector2<T> &v) const {
+        DCHECK(!v.HasNaNs());
+        return Point2<T>(x + v.x, y + v.y);
+    }
+    Point2<T> &operator+=(const Point2<T> &p) {
+        DCHECK(!p.HasNaNs());
+        x += p.x;
+        y += p.y;
+        return *this;
+    }
+    Point2<T> &operator+=(const Vector2<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x += v.x;
+        y += v.y;
+        return *this;
+    }
+    Vector2<T> operator-(const Point2<T> &p) const {
+        DCHECK(!p.HasNaNs());
+        return Vector2<T>(x - p.x, y - p.y);
+    }
+    Point2<T> operator-(const Vector2<T> &v) const {
+        DCHECK(!v.HasNaNs());
+        return Point2<T>(x - v.x, y - v.y);
+    }
+    Point2<T> &operator-=(const Vector2<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x -= v.x;
+        y -= v.y;
+        return *this;
+    }
+    Point2<T> operator-() const { return Point2<T>(-x, -y); }
+    template <typename U>
+    Point2<T> operator*(U u) const {
+        DCHECK(!isNaN(u));
+        return Point2<T>(x * u, y * u);
+    }
+    template <typename U>
+    Point2<T> operator*=(U u) {
+        DCHECK(!isNaN(u));
+        x *= u;
+        y *= u;
+        return *this;
+    }
+    template <typename U>
+    Point2<T> operator/(U u) const {
+        DCHECK(!isNaN(u));
+        Float inv = (Float)1 / u;
+        return Point2<T>(x * inv, y * inv);
+    }
+    template <typename U>
+    Point2<T> operator/=(U u) {
+        DCHECK(!isNaN(u));
+        Float inv = (Float)1 / u;
+        x *= inv;
+        y *= inv;
+        return *this;
+    }
+    T operator[](int i) const {
+        DCHECK(i >= 0 && i <= 1);
+        if(i == 0) return x;
+        return y;
+    }
+    T &operator[](int i) {
+        DCHECK(i >= 0 && i <= 1);
+        if(i == 0) return x;
+        return y;
+    }
+    bool operator==(const Point2<T> &p) const { return x == p.x && y == p.y; }
+    bool operator!=(const Point2<T> &p) const { return x != p.x || y != p.y; }
 
-    T x,y;
+    T x, y;
 };
+
+template <typename T>
+inline std::ostream &operator<<(std::ostream &os, const Point2<T> &p) {
+    os << "[" << p.x << ", " << p.y << "]";
+    return os;
+}
+
+inline std::ostream &operator<<(std::ostream &os, const Point2<Float> &p) {
+    os << StringPrintf("[%f, %f]", p.x, p.y);
+    return os;
+}
+
+template <typename T>
+class Point3 {
+public:
+    Point3(): x(0), y(0), z(0){}
+    Point3(T x, T y, T z): x(x), y(y), z(z){ DCHECK(!HasNaNs()); }
+    Point3(const Point3<T>& p): x(p.x), y(p.y), z(p.z) { DCHECK(!HasNaNs()); }
+    template <typename U>
+    explicit Point3(const Point3<U>& p) {
+        x = (T)p.x;
+        y = (T)p.y;
+        z = (T)p.z;
+        DCHECK(!HasNaNs());
+    }
+    bool HasNaNs() const { return isNaN(x) || isNaN(y) || isNaN(z); }
+    template <typename U>
+    explicit operator Vector3<U>() const {
+        return Vector3<U>(x, y, z);
+    }
+    Point3<T> &operator=(const Point3<T> &p) {
+        x = p.x;
+        y = p.y;
+        z = p.z;
+        return *this;
+    }
+    Point3<T> operator+(const Point3<T> &p) const {
+        DCHECK(!p.HasNaNs());
+        return Point3<T>(x + p.x, y + p.y, z + p.z);
+    }
+    Point3<T> operator+(const Vector3<T> &v) const {
+        DCHECK(!v.HasNaNs());
+        return Point3<T>(x + v.x, y + v.y, z + v.z);
+    }
+    Point3<T> &operator+=(const Point3<T> &p) {
+        DCHECK(!p.HasNaNs());
+        x += p.x;
+        y += p.y;
+        z += p.z;
+        return *this;
+    }
+    Point3<T> &operator+=(const Vector3<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x += v.x;
+        y += v.y;
+        z += v.y;
+        return *this;
+    }
+    Vector3<T> operator-(const Point3<T> &p) const {
+        DCHECK(!p.HasNaNs());
+        return Vector3<T>(x - p.x, y - p.y, z - p.z);
+    }
+    Point3<T> operator-(const Vector3<T> &v) const {
+        DCHECK(!v.HasNaNs());
+        return Point3<T>(x - v.x, y - v.y, z - v.z);
+    }
+    Point3<T> operator-=(const Vector3<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return *this;
+    }
+    Point3<T> operator-() const { return Point3<T>(-x, -y); }
+    template <typename U>
+    Point3<T> operator*(U u) const {
+        DCHECK(!isNaN(u));
+        return Point3<T>(x * u, y * u, z *u);
+    }
+    template <typename U>
+    Point3<T> &operator*=(U u) {
+        DCHECK(!isNaN(u));
+        x *= u;
+        y *= u;
+        z *= u;
+        return *this;
+    }
+    template <typename U>
+    Point3<T> operator/(U u) const {
+        DCHECK(!isNaN(u));
+        Float inv = (Float)1 / u;
+        return Point3<T>(x * inv, y * inv, z * inv);
+    }
+    template <typename U>
+    Point3<T> &operator/=(U u) {
+        DCHECK(!isNaN(u));
+        Float inv = (Float)1 / u;
+        x *= u;
+        y *= u;
+        z *= u;
+        return *this;
+    }
+    T operator[](int i) const {
+        DCHECK(i >= 0 && i <= 2);
+        if(i == 0) return x;
+        else if(i == 1) return y;
+        else return z;
+    }
+    T &operator[](int i) {
+        DCHECK(i >= 0 && i <= 2);
+        if(i == 0) return x;
+        else if(i == 1) return y;
+        else return z;
+    }
+    bool operator==(const Point3<T> &p) const { return x == p.x && y == p.y && z == p.z; }
+    bool operator!=(const Point3<T> &p) const { return x != p.x || y != p.z || z != p.z; }
+    T x, y, z;
+};
+
+template <typename T>
+inline std::ostream &operator<<(std::ostream &os, const Point3<T> &p) {
+    os << "[" << p.x << ", " << p.y << ", " << p.z << "]";
+    return os;
+}
+
+inline std::ostream &operator<<(std::ostream &os, const Point3<Float> &p) {
+    os << StringPrintf("[%f, %f, %f]", p.x, p.y, p.z);
+    return os;
+}
 
 typedef Point2<int> Point2i;
 typedef Point2<Float> Point2f;
+typedef Point3<int> Point3i;
+typedef Point3<Float> Point3f;
 
 template <typename T>
 Vector2<T>::Vector2(const Point2<T> &p): x(p.x), y(p.y) {
--- src/tests/geometry.cpp
+++ src/tests/geometry.cpp
@@ -41,6 +41,12 @@ inline bool compare_vector_float(const Vector2f &v1, const Vector2f &v2) {
     return compare_float(r.x, 0.0f) && compare_float(r.y, 0.0f); 
 }
 
+inline bool compare_point_float(const Point2f &v1, const Point2f& p2) {
+    Vector2f r = v1 - p2;
+    return compare_float(r.x, 0.0f) && compare_float(r.y, 0.0f); 
+}
+
+
 inline bool compare_vector_float(const Vector3f &v1, const Vector3f &v2) {
     Vector3f r = v1 - v2;
     return compare_float(r.x, 0.0f) && compare_float(r.y, 0.0f) && compare_float(r.z, 0); 
@@ -162,8 +168,96 @@ TEST(Vector3f, OperateTest) {
     EXPECT_TRUE(compare_float(v1.Length(), std::sqrt(Float(v1.x * v1.x + v1.y * v1.y + v1.z * v1.z))));
 }
 
-TEST(Point2i, OperateTest) {
-    Point2i p1(1,2);
-    Vector2i v1(p1);
+TEST(Point2f, OperateTest) {
+    Float begin = -1000.0;
+    Float end = 1000.0;
+    Float x0 = get_random_float(begin, end);
+    Float y0 = get_random_float(begin, end);
+    Float x1 = get_random_float(begin, end);
+    Float y1 = get_random_float(begin, end);
+    Float u = get_random_float(begin, end);
+
+
+    Point2f p0(x0, y0);
+    Point2f v1(x1, y1);
+    Vector2f v(get_random_float(begin, end), get_random_float(begin, end));
+
+    Vector2i v2 = (Vector2i)p0;
+    EXPECT_EQ(v2, Vector2i((int)p0.x, (int)p0.y));
+
+    Point2i p = (Point2i)v;
+    EXPECT_EQ(p, Point2i((int)v.x, (int)v.y));
+
+    p = (Point2i)v1;
+    EXPECT_EQ(p, Point2i((int)v1.x, (int)v1.y));
+
+    EXPECT_NE(p0, v1);
+    EXPECT_TRUE(compare_point_float(p0 + v1, Point2f(p0.x + v1.x, p0.y + v1.y)));
+    EXPECT_TRUE(compare_point_float(p0 += v1, Point2f(p0.x + v1.x, p0.y + v1.y)));
+    EXPECT_TRUE(compare_point_float(p0 + v, Point2f(p0.x + v.x, p0.y + v.y)));
+    EXPECT_TRUE(compare_point_float(v1 += v, Point2f(v1.x + v.x, v1.y + v.y)));
+
+    EXPECT_TRUE(compare_vector_float(p0 - v1, Vector2f(p0.x - v1.x, p0.y - v1.y)));
+    EXPECT_TRUE(compare_point_float(p0 - v, Point2f(p0.x - v.x, p0.y - v.y)));
+    EXPECT_TRUE(compare_point_float(v1 -= v, Point2f(v1.x - v.x, v1.y - v.y)));
+
+    EXPECT_TRUE(compare_point_float(p0 * u, Point2f(p0.x * u, p0.y * u)));
+    EXPECT_TRUE(compare_point_float(v1 *= u, Point2f(v1.x * u, v1.y * u)));
+
+      
+    EXPECT_TRUE(compare_point_float(p0 / u, Point2f(p0.x / u, p0.y / u)));
+    EXPECT_TRUE(compare_point_float(v1 /= u, Point2f(v1.x / u, v1.y / u)));
+
+    EXPECT_TRUE(compare_point_float(-p0, Point2f(-p0.x, -p0.y)));
+    EXPECT_TRUE(compare_float(p0[0], p0.x));
+    EXPECT_TRUE(compare_float(p0[1], p0.y));
+}
+
+
+TEST(Point3f, OperateTest) {
+    // Float begin = -1000.0;
+    // Float end = 1000.0;
+    // Float x0 = get_random_float(begin, end);
+    // Float y0 = get_random_float(begin, end);
+    // Float z0 = get_random_float(begin, end);
+    // Float x1 = get_random_float(begin, end);
+    // Float y1 = get_random_float(begin, end);
+    // Float z1 = get_random_float(begin, end);
+    // Float u = get_random_float(begin, end);
+
+
+    // Point3f p0(x0, y0, z0);
+    // Point3f v1(x1, y1, z1);
+    // Vector2f v(get_random_float(begin, end), get_random_float(begin, end));
+
+    // Vector2i v2 = (Vector2i)p0;
+    // EXPECT_EQ(v2, Vector2i((int)p0.x, (int)p0.y));
+
+    // Point2i p = (Point2i)v;
+    // EXPECT_EQ(p, Point2i((int)v.x, (int)v.y));
+
+    // p = (Point2i)v1;
+    // EXPECT_EQ(p, Point2i((int)v1.x, (int)v1.y));
+
+    // EXPECT_NE(p0, v1);
+    // EXPECT_TRUE(compare_point_float(p0 + v1, Point2f(p0.x + v1.x, p0.y + v1.y)));
+    // EXPECT_TRUE(compare_point_float(p0 += v1, Point2f(p0.x + v1.x, p0.y + v1.y)));
+    // EXPECT_TRUE(compare_point_float(p0 + v, Point2f(p0.x + v.x, p0.y + v.y)));
+    // EXPECT_TRUE(compare_point_float(v1 += v, Point2f(v1.x + v.x, v1.y + v.y)));
+
+    // EXPECT_TRUE(compare_vector_float(p0 - v1, Vector2f(p0.x - v1.x, p0.y - v1.y)));
+    // EXPECT_TRUE(compare_point_float(p0 - v, Point2f(p0.x - v.x, p0.y - v.y)));
+    // EXPECT_TRUE(compare_point_float(v1 -= v, Point2f(v1.x - v.x, v1.y - v.y)));
+
+    // EXPECT_TRUE(compare_point_float(p0 * u, Point2f(p0.x * u, p0.y * u)));
+    // EXPECT_TRUE(compare_point_float(v1 *= u, Point2f(v1.x * u, v1.y * u)));
+
+      
+    // EXPECT_TRUE(compare_point_float(p0 / u, Point2f(p0.x / u, p0.y / u)));
+    // EXPECT_TRUE(compare_point_float(v1 /= u, Point2f(v1.x / u, v1.y / u)));
+
+    // EXPECT_TRUE(compare_point_float(-p0, Point2f(-p0.x, -p0.y)));
+    // EXPECT_TRUE(compare_float(p0[0], p0.x));
+    // EXPECT_TRUE(compare_float(p0[1], p0.y));
 }
 
