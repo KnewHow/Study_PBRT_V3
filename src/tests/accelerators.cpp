30c90ebb12703f90e262ffaa809e5f8b17030477
Knewhow <how.yuangh@gmail.com>
2021-10-26 14:11:20 +0800

    commit test

--- src/tests/accelerators.cpp
+++ src/tests/accelerators.cpp
@@ -118,6 +118,17 @@ void test_bvh_insersect(const std::shared_ptr<BVHAccel> bvh, const std::vector<R
     end = getCurrentMilliseconds();
 }
 
+void test_bvh_insersectP(const std::shared_ptr<BVHAccel> bvh, const std::vector<Ray> rays, std::chrono::milliseconds &begin, std::chrono::milliseconds &end) {
+    begin = getCurrentMilliseconds();
+    int hitTimes = 0;
+    for(const Ray &ray: rays) {
+        bool isHit = bvh->IntersectP(ray);
+        if(isHit) ++hitTimes;
+    }
+    LOG(INFO) << "test_bvh_insersectP result, hit times: " << hitTimes << ", no hit times: " << rays.size() - hitTimes;
+    end = getCurrentMilliseconds();
+}
+
 std::shared_ptr<BVHAccel> buildBVH(const std::vector<std::shared_ptr<Primitive>> &ps, BVHAccel::SplitMethod method, std::chrono::milliseconds &begin, std::chrono::milliseconds &end) {
     begin = getCurrentMilliseconds();
     std::shared_ptr<BVHAccel> bvh = std::make_shared<BVHAccel>(ps, method);
@@ -148,8 +159,6 @@ void generateTestRays2(std::vector<Ray> &rays) {
     }
 }
 
-
-
 TEST(BVHAccel, ComparePerformance) {
     std::vector<std::shared_ptr<Primitive>> ps;
     std::chrono::milliseconds begin, end;
@@ -166,7 +175,7 @@ TEST(BVHAccel, ComparePerformance) {
     auto bvh_sah = buildBVH(ps, BVHAccel::SplitMethod::SAH, begin, end);
     printTime("Build BVH with SAH took: ", begin, end);
     
-    LOG(INFO) << "-------------------------------------------";
+    LOG(INFO) << "---------------------test insersect----------------------------";
     
     //std::vector<Ray> rays = generateTestRays();
     std::vector<Ray> rays;
@@ -180,5 +189,15 @@ TEST(BVHAccel, ComparePerformance) {
 
     test_bvh_insersect(bvh_sah, rays, begin, end);
     printTime("Test BVH with SAH took: ", begin, end);
-    
+
+    LOG(INFO) << "---------------------test insersectP----------------------------";
+
+    test_bvh_insersectP(bvh_middle, rays, begin, end);
+    printTime("Test BVH with Millde took: ", begin, end);
+
+    test_bvh_insersectP(bvh_equalCounts, rays, begin, end);
+    printTime("Test BVH with EqualCounts took: ", begin, end);
+
+    test_bvh_insersectP(bvh_sah, rays, begin, end);
+    printTime("Test BVH with SAH took: ", begin, end);
 } 
